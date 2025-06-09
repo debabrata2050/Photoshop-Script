0282f011855a66156460eae1316c7726e4f27243
Debabrata Halder <99135144+debabrata2050@users.noreply.github.com>
2025-06-09 15:16:28 +0530

    Update Remove All Guide Lines.jsx

--- Remove All Guide Lines.jsx
+++ Remove All Guide Lines.jsx
@@ -1,5 +1,20 @@
 #target photoshop
 
+/*
+ * Author: Debabrata Halder
+ * GitHub: https://github.com/debabrata2050
+ *
+ * Date Created: 2025-06-09
+ * Description:
+ *     This script removes all guides from the currently active Photoshop document.
+ *     It's useful for quickly clearing layout aids without manually deleting them.
+ *
+ * Instructions:
+ * - Make sure a document is open.
+ * - Run the script to delete all vertical and horizontal guides.
+ *
+ */
+
 if (app.documents.length === 0) {
     alert("Please open a document first.");
 } else {
